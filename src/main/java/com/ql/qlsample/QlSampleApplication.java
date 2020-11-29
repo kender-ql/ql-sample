d2ef7a44721a814dc6857908737f4b58383af5bc
kender <1421894128@qq.com>
2020-11-29 11:02:20 +0800

    1. 项目初始化

---
+++ src/main/java/com/ql/qlsample/QlSampleApplication.java
@@ -0,0 +1,13 @@
+package com.ql.qlsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlSampleApplication.class, args);
+    }
+
+}
